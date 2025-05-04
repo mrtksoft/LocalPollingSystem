3e4728d0103f52e81602a2b11c3199489ea155ca
mrtksoft <11276508+mrtksoft@users.noreply.github.com>
2025-05-04 14:17:38 +0300

    Update app.py

--- vote_service/app.py
+++ vote_service/app.py
@@ -32,7 +32,7 @@ def cast_vote(poll_id):
         ##Simple Duplicate vote check using IP address
         # very basic, not bulletproof solution
         #voter_id = request.remote_addr # Getting clients IP address
-        vote_id = data.get('userIP')
+        voter_id = data.get('userIP')
 
         conn = database.get_db()
         cursor = conn.cursor()
